405475b709e3972a2af97251097fbf453fbc3dd6
Hariel <hariel@hotmail.com>
2021-03-24 13:10:11 -0300

    dados

---
+++ Nodejs/dados.js
@@ -0,0 +1,31 @@
+const dadosjira = require('./app/dadosJSON/jira.json')
+const dadostrello = require('./app/dadosJSON/trello.json')
+const dados = dadosjira.concat(dadostrello)
+
+let inputstats = false
+const finalizado = dados => dados.finished == inputstats // buscando por card já finalizados
+
+let inputprojeto = "[Souza Comércio e Associados] - Innovative background implementation"
+const projeto = projeto => projeto.project ==  inputprojeto // buscando pelo projeto souza comércio
+
+
+
+let nomeEsobrenome = function(nome, sobrenome) {
+    return nome + sobrenome 
+}
+
+let inputnome = nomeEsobrenome('Gustavo', 'Santos')
+const programador = nome => nome.user.first_name + nome.user.last_name == inputnome
+
+
+// cardjira = '0db2dfdc-a4b8-4055-b2ad-6655c06a7663' // cardtrello = '261fe1be-db29-4c0c-b423-ea964ff4fbb0'
+let inputcard = '0db2dfdc-a4b8-4055-b2ad-6655c06a7663'
+const numerodocard = n => n.id == inputcard
+
+
+// console.log(dados)
+// console.log(dados.filter(programador))
+console.log(dados.filter(projeto))
+
+
+module.exports = { finalizado, projeto, programador, numerodocard, dados }
