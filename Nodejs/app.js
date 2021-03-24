bb6d96f7592d77932feb5e8250fd18499db9ef6b
Hariel <hariel@hotmail.com>
2021-03-24 07:54:12 -0300

    express

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,8 +1,6 @@
 const app = require('./config/server')
 const port = 3000
 
-const rotahome = require('./app/routes/home')
-rotahome(app)
 
 app.listen(port, () => {
     console.log(`Servidor rodando com Express na porta: ${port}.`)
--- Nodejs/config/server.js
+++ Nodejs/config/server.js
@@ -1,8 +1,4 @@
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'ejs');
-
-app.set('views', './app/views')
-
 module.exports = app
