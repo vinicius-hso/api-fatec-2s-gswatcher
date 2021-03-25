f1aa057f79d6dd334f85314fe0596a90f5db4dbd
Hariel <hariel@hotmail.com>
2021-03-25 11:38:10 -0300

    update dados

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,10 +1,15 @@
 const app = require('./config/server'), port = 3000
 
 
-// const dados = require('./dados')
+const dados = require('./dados')
 // console.log(dados)
 
 
+app.get('/', (req, res) => res.send(programador('Antônio', 'Nogueira')))
+
+
+
+
 app.listen(port, () => {
-    console.log(`Servidor rodando com Express na porta: ${port}.`)
+    console.log(`Servidor rodando com Express na url: localhost:${port}.`)
 })
--- Nodejs/dados.js
+++ Nodejs/dados.js
@@ -2,37 +2,54 @@ const dadosjira = require('./app/dadosJSON/jira.json')
 const dadostrello = require('./app/dadosJSON/trello.json')
 const dados = dadostrello.concat(dadosjira)
 
-let inputstats = false
-const finalizado = dados => dados.finished == inputstats // buscando por card já finalizados
 
+// Buscando card pelo status final
+function projetosFinalizados(input) {
+    let inputstats = input
+    const finalizado = dados => dados.finished == inputstats || dados.isFinished == inputstats // buscando por card já finalizados
 
-
-let inputprojeto = "[Souza Comércio e Associados] - Innovative background implementation"
-const projeto = projeto => projeto.project ==  inputprojeto // buscando pelo projeto souza comércio
-
-
-
-const nomeEsobrenome = function(nome, sobrenome) {
-    return nome[0].toLocaleUpperCase() + nome.slice(1) + sobrenome[0].toLocaleUpperCase() + sobrenome.slice(1) // definindo letras maiusculas, para evitar erro de busca.
+    return console.log(dados.filter(finalizado))
 }
 
-let inputnome = nomeEsobrenome('gustavo', 'santos')
-const programador = nome => nome.user.first_name + nome.user.last_name == inputnome
+
+
+// buscando pelo projeto especifico
+function buscandoProjeto(input) {
+    let inputprojeto = input
+    const projeto = projeto => projeto.project ==  inputprojeto 
+
+    return console.log(dados.filter(projeto))
+}
+
+
+
+// Buscando por card com o programador X
+function programador(nome, sobrenome) {
+    let inputnome = nome[0].toLocaleUpperCase() + nome.slice(1) + sobrenome[0].toLocaleUpperCase() + sobrenome.slice(1) // definindo letras maiusculas, para evitar erro de busca.
+    const buscandoprogramador = nome => nome.user.first_name + nome.user.last_name == inputnome
+    
+    return console.log(dados.filter(buscandoprogramador))
+}
+
+
+
+// buscando numero de card especifico.
+function cardID(input) {
+    // cardjira = '0db2dfdc-a4b8-4055-b2ad-6655c06a7663' // cardtrello = "0ac33506-5610-418f-9b7b-fe9eaacf4f53"
+    let inputcard = input
+    const numerodocard = n => n.id == inputcard || n._id == inputcard
+
+    return console.log(dados.filter(numerodocard))
+}
+
+
+
+// projetosFinalizados(false)
+// cardID("0ac33506-5610-418f-9b7b-fe9eaacf4f53")
+// programador('Antônio', 'Nogueira')
+// buscandoProjeto("[Souza Comércio e Associados] - Innovative background implementation")
 
 
 
 
-// cardjira = '0db2dfdc-a4b8-4055-b2ad-6655c06a7663' // cardtrello = '261fe1be-db29-4c0c-b423-ea964ff4fbb0'
-let inputcard = '0db2dfdc-a4b8-4055-b2ad-6655c06a7663'
-const numerodocard = n => n.id == inputcard
-
-
-
-
-// console.log(dados)
-// console.log(dados.filter(programador))
-// console.log(dados.filter(projeto))
-// console.log(dados.filter(numerodocard))
-
-
-module.exports = { finalizado, projeto, programador, numerodocard, dados }
+module.exports = { dados, buscandoProjeto, programador, cardID }
