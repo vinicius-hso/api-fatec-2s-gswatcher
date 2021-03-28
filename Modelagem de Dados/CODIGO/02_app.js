08f51f690a6ee5092d66b05ee4cff749d28d9f9e
mariana299 <64920629+mariana299@users.noreply.github.com>
2021-03-28 20:28:22 -0300

    Add files via upload

---
+++ Modelagem de Dados/CODIGO/02_app.js
@@ -0,0 +1,25 @@
+const app = require('./config/server'), port = 3000
+const dados = require('./dados')
+
+
+const bodyParser = require('body-parser')
+const db = require('./populating_db')
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+var resposta = db(dados);
+console.log(resposta)
+
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando com Express na url: localhost:${port}.`)
+})
