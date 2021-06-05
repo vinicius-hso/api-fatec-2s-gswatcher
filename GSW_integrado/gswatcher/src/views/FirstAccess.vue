849d1fc366064ef55496d5f63a16fbdffbcc950b
Debora Faria <deeborafaria@gmail.com>
2021-06-05 20:19:26 -0300

    Updated link: First Access video

--- GSW_integrado/gswatcher/src/views/FirstAccess.vue
+++ GSW_integrado/gswatcher/src/views/FirstAccess.vue
@@ -21,7 +21,7 @@
               <iframe
                 width="280px"
                 height="200px"
-                src="https://www.youtube.com/embed/KOmHkYIDPMU"
+                src="https://www.youtube.com/embed/8j8ktkdrvd4"
                 title="YouTube video player"
                 frameborder="1"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
